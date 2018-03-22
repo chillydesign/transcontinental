2341cb0e46b54508db3fbd1476ad35c209f4a609
Charles Harvey <harvey.charles@gmail.com>
2018-03-22 19:50:59 +0000

    updated facebook share image

--- functions.php
+++ functions.php
@@ -618,14 +618,14 @@ function social_meta_properties(){
         $smp->description = $excerpt;
         $smp->image =  thumbnail_of_post_url( $post_id, 'large' );
         if ($smp->image == '') {
-            $smp->image =   get_template_directory_uri() . '/img/trans.png';
+            $smp->image =   get_template_directory_uri() . '/img/transcontinental_share.jpg';
         }
         $smp->url = get_the_permalink();
 
     } else {
         $smp->title =    get_bloginfo('name');
         $smp->description = get_bloginfo('description');
-        $smp->image =   get_template_directory_uri() . '/img/trans.png';
+        $smp->image =   get_template_directory_uri() . '/img/transcontinental_share.jpg';
         $smp->url = get_home_url();
     }
 
